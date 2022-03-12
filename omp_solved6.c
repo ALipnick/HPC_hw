7536732e1599e96bcb26d16bd21de20a4ffd4493
ALipnick <adl592@nyu.edu>
2022-03-12 16:55:52 -0500

    solved 6

--- omp_solved6.c
+++ omp_solved6.c
@@ -19,12 +19,13 @@ int i,tid;
 float sum;
 
 tid = omp_get_thread_num();
-#pragma omp for reduction(+:sum)
+#pragma omp for 
   for (i=0; i < VECLEN; i++)
     {
     sum = sum + (a[i]*b[i]);
     printf("  tid= %d i=%d\n",tid,i);
     }
+    return sum;
 }
 
 
@@ -36,8 +37,8 @@ for (i=0; i < VECLEN; i++)
   a[i] = b[i] = 1.0 * i;
 sum = 0.0;
 
-#pragma omp parallel shared(sum)
-  dotprod();
+#pragma omp parallel reduction(+:sum)
+  sum = dotprod();
 
 printf("Sum = %f\n",sum);
 
