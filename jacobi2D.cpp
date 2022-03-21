c94bd98df98c104ead31c42c12ccb0a78389ec6a
ALipnick <adl592@nyu.edu>
2022-03-20 21:30:24 -0400

    make file jacobi

--- jacobi2D.cpp
+++ jacobi2D.cpp
@@ -1,6 +1,7 @@
 //things to include
 #include "utils.h"
-#include <math.h>
+//#include <math.h>
+#include <cmath>
 #include <omp.h>
 
 
@@ -8,8 +9,13 @@ int main(int argc, char** argv){
 	int max_iter = 50000;
 	int res_factor = 1000;
 
-	int N = read_option<long>("-N", argc, argv);
-	int num_of_threads = read_option<long>("-T", argc, argv);
+	//int N = read_option<long>("-N", argc, argv);
+	//int num_of_threads = read_option<long>("-T", argc, argv);
+	//int max_num_threads = omp_get_num_threads();
+	//num_of_threads = min(num_of_threads, max_num_threads);
+	
+	int N = 100;
+	int num_of_threads = 16;
 	
 	double* u = (double*) malloc((N+2) * (N+2) * sizeof(double)); // N+2 x N+2 grid with 0 boundary
 	double* u_new = (double*) malloc((N+2) * (N+2) * sizeof(double));	
@@ -30,7 +36,7 @@ int main(int argc, char** argv){
 	for (iter = 0; iter < max_iter; iter++) {//iterate
 		//update u using jacobi
 		//loop through and solve for u_new with u
-		#pragma omp parallel for collapse (2) num_threads(num_of_threads)
+		#pragma omp parallel for collapse(2) num_threads(num_of_threads)
 		for (int i = 1; i<N+1; i++){
 			for (int j = 1; j<N+1; j++){
 				u_new[i+j*(N+2)] = 0.25*(h_2 
@@ -42,7 +48,7 @@ int main(int argc, char** argv){
 		
 		//now loop through and calc residual and set u to u_new
 		residual = 0;
-		#pragma omp parallel for collapse (2) reduction(+:residual)
+		#pragma omp parallel for collapse(2) reduction(+:residual) num_threads(num_of_threads)
 		for (int i = 1; i<N+1; i++){
 			for (int j = 1; j<N+1; j++){
 				u[i+j*(N+2)] = u_new[i+j*(N+2)];
