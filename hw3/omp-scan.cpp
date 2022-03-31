bfc6dd19cf859eaf54cac555808cd1186003989a
ALipnick <adl592@nyu.edu>
2022-03-31 14:56:16 -0400

    solved scan, added writeup

--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -15,6 +15,41 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
 
 void scan_omp(long* prefix_sum, const long* A, long n) {
   // TODO: implement multi-threaded OpenMP scan
+  // each thread sums its section
+  // then waits until the section before it is done with its last value
+  // and adds that section's last value to all of it values
+  // starting with it's last value so the next section can start sooner
+  int num_of_threads = 16; //assume N is dividible by num_threads
+  //                      so possible values are 1,2,4,5,8,10,16
+  //otherwise would have to do some work to separate
+  long k = floor(n/num_of_threads); //how many terms each thread takes
+  #pragma omp parallel num_threads(num_of_threads)
+  {
+    int thread_num = omp_get_thread_num();
+    if (thread_num == 0){
+      prefix_sum[0] = 0;
+    }
+    else{
+      prefix_sum[thread_num * k] = A[thread_num * k -1]; //set prefix_sum[thread_num * k] = A[thread_num*k]
+    }
+    for (long i = thread_num*k+1; i < (thread_num+1)*k; ++i) {
+      prefix_sum[i] = prefix_sum[i-1] + A[i-1]; //add A[i-1]
+    }
+    #pragma omp barrier //don't add results to next thread's values unil all are done
+
+    if (thread_num == 0){
+      for (long i = 1; i<num_of_threads; ++i){
+            prefix_sum[(i+1)*k-1] = prefix_sum[(i+1)*k-1] + prefix_sum[i*k-1];
+      }
+    }
+    #pragma omp barrier //after adding to last value we can do the  rest in parallel
+
+    if (thread_num > 0){
+         for (long i = thread_num*k; i < (thread_num+1)*k - 1; ++i) {
+                  prefix_sum[i] = prefix_sum[i] + prefix_sum[thread_num*k-1];
+         }
+    }
+  }
 }
 
 int main() {
