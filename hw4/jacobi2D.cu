8a6a8885969815f3ddf344dea0488111ae41e155
ALipnick <adl592@nyu.edu>
2022-04-18 18:35:11 -0400

    did most of jacobi

--- hw4/jacobi2D.cu
+++ hw4/jacobi2D.cu
@@ -1,73 +1,129 @@
-//things to include
-#include "utils.h"
-#include <cmath>
+// $ nvcc -arch=sm_61 jacobi2D.cu -o jacobi2D -Xcompiler -fopenmp
+// flag -Xcompiler passes next flag directly to compiler
+#include <algorithm>
+#include <stdio.h>
 #include <omp.h>
+#include <string>
+#include <random>
 
-int main(int argc, char** argv){
-	int max_iter = 5000;
-	int res_factor = 10000;
+#define BLOCK_SIZE 32
 
-	//int N = read_option<long>("-N", argc, argv);
-	//int num_of_threads = read_option<long>("-T", argc, argv);
-	int N = 500;
-	int num_of_threads = 16;
+void jacobi2D_step(double* u_new, const double* u, double h, long N) {
+  #pragma omp parallel for collapse(2)
+  for (int i = 1; i<N+1; i++){
+  	for (int j = 1; j<N+1; j++){
+  		u_new[i+j*(N+2)] = 0.25*(h*h 
+			+ u[(i-1)+(j)*(N+2)] + u[(i)+(j-1)*(N+2)]
+			+ u[(i+1)+(j)*(N+2)] + u[(i)+(j+1)*(N+2)] );
+			}
+		}
+}
 
-	int max_num_threads = omp_get_max_threads();
-	num_of_threads = std::min(num_of_threads, max_num_threads);
-	
-	double* u = (double*) malloc((N+2) * (N+2) * sizeof(double)); // N+2 x N+2 grid with 0 boundary
-	double* u_new = (double*) malloc((N+2) * (N+2) * sizeof(double));	
-	for (long i = 0; i < (N+2)*(N+2); i++){ //initialize values
+
+__global__ 
+void jacobi2D_step_kernel(double* u_new, const double* u, double h, long N) {
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  int j = blockDim.y * blockIdx.y + threadIdx.y;
+
+  if ( (i < N+1) && (j < N+1) && (i > 0) && (j > 0) ) {
+    u_new[i+j*(N+2)] = 0.25*( h*h+ u[i-1+j*(N+2)]
+			  + u[i+(j-1)*(N+2)] + u[i+1+j*(N+2)] + u[i+(j+1)*(N+2)] );
+  }
+}
+
+void jacobi2D(double* u_new, double* u,  double* temp,double h, long N, long max_iter) {
+	for (long iter = 0; iter < max_iter; iter++) {
+		jacobi2D_step(u_new,  u,  h, N);
+		temp = u;
+    	u = u_new;
+    	u_new = temp;
+	}
+}
+
+
+void jacobi2D_cuda(double* u_new, double* u, double* temp, double h, long N, long max_iter) {
+  dim3 BlockDim(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 GridDim(N/BLOCK_SIZE, N/BLOCK_SIZE);
+  for (long iter = 0; iter < max_iter; iter++) {
+  	jacobi2D_step_kernel<<<GridDim, BlockDim>>>(u_new,  u,  h, N);
+  	temp = u;
+    u = u_new;
+    u_new = temp;
+	}
+}
+
+
+
+void Check_CUDA_Error(const char *message){
+  cudaError_t error = cudaGetLastError();
+  if(error!=cudaSuccess) {
+    fprintf(stderr,"ERROR: %s: %s\n", message, cudaGetErrorString(error) );
+    exit(-1);
+  }
+}
+
+
+int main(void) {
+  int max_iter = 1000;
+  int N = 500;
+ 
+  double* u = (double*) malloc((N+2) * (N+2) * sizeof(double)); // N+2 x N+2 grid with 0 boundary
+  double* u_new = (double*) malloc((N+2) * (N+2) * sizeof(double));
+  double* temp = (double*) malloc((N+2) * (N+2) * sizeof(double));
+  double* u_ref = (double*) malloc((N+2) * (N+2) * sizeof(double));
+  #pragma omp parallel for schedule(static)
+  for (long i = 0; i < (N+2)*(N+2); i++){ //initialize values
 		u[i] = 0;
 		u_new[i] = 0;
 	}
-	
-	//calc necessary info
-	double h_2 = ((double)1)/((N+1)*(N+1)); // h^2
-	double residual = N; //calc starting residual
-	double tolerance = residual/res_factor; //calc tolerance to compare against
-	printf("\ninitial residual = %f so tolerance = %f\n",residual, tolerance); 
-	int iter;
-	
-	Timer t; // so we can time how long everything takes
-	t.tic(); //start timer
-	for (iter = 0; iter < max_iter; iter++) {//iterate
-		//update u using jacobi
-		//loop through and solve for u_new with u
-		#ifdef _OPENMP
-		#pragma omp parallel for collapse(2) num_threads(num_of_threads)
-		#endif
-		for (int i = 1; i<N+1; i++){
-			for (int j = 1; j<N+1; j++){
-				u_new[i+j*(N+2)] = 0.25*(h_2 
-					+ u[(i-1)+(j)*(N+2)] + u[(i)+(j-1)*(N+2)]
-					+ u[(i+1)+(j)*(N+2)] + u[(i)+(j+1)*(N+2)] );
-			}
-			
-		}
-		
-		//now loop through and calc residual and set u to u_new
-		residual = 0;
-		#ifdef _OPENMP
-		#pragma omp parallel for collapse(2) reduction(+:residual) num_threads(num_of_threads)
-		#endif
-		for (int i = 1; i<N+1; i++){
-			for (int j = 1; j<N+1; j++){
-				u[i+j*(N+2)] = u_new[i+j*(N+2)];
-				residual += pow(1 -(((double)4*u_new[(i)+(j)*(N+2)]
-					- u_new[(i-1)+(j)*(N+2)] - u_new[(i)+(j-1)*(N+2)]
-				 	- u_new[(i+1)+(j)*(N+2)] - u_new[(i)+(j+1)*(N+2)])/h_2) ,2);
-			}
-		}
-		residual = pow(residual,0.5);
-		if (residual < tolerance){ //if below tolerance stop iterating
-			break;
-		}
-	}
-	double final_time = t.toc(); //store final time
-	//print results
-   	printf("after %d iterations the residual is %f\n", iter, residual);
-   	printf("Total time was %fs with a total of %d threads\n",final_time,num_of_threads);
-   	free(u_new); //free because malloc
-   	free(u);
+  //calc necessary info
+  double h = ((double)1)/((N+1)*(N+1)); // h^2
+  
+
+
+  double tt = omp_get_wtime();
+  jacobi2D(u_new, u, temp, h, N, max_iter);
+  for ( long i = 0; i < (N+2)*(N+2); i++ ) {
+   u_ref[i] = u[i];
+  }
+  printf("CPU %f s\n", omp_get_wtime()-tt);
+
+  double *u_d, *u_new_d;
+  cudaMalloc(&u_d, (N+2) * (N+2) *sizeof(double));
+  Check_CUDA_Error("malloc x failed");
+  cudaMalloc(&u_new_d, (N+2) * (N+2) *sizeof(double));
+
+  // reset u values
+  #pragma omp parallel for schedule(static)
+  for (long i = 0; i < (N+2)*(N+2); i++){ //initialize values
+		u[i] = 0;
+		u_new[i] = 0;
+  }
+
+  tt = omp_get_wtime();
+  cudaMemcpy(u_d, u, (N+2) * (N+2) *sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(u_new_d, u_new, (N+2) * (N+2) * sizeof(double), cudaMemcpyHostToDevice);
+
+  double ttinner = omp_get_wtime();
+  jacobi2D_cuda(u_new, u, temp, h, N,max_iter);
+  cudaDeviceSynchronize();
+  ttinner = omp_get_wtime() - ttinner;
+  cudaMemcpy(u_new, u_new_d, (N+2) * (N+2) * sizeof(double), cudaMemcpyDeviceToHost);
+  printf("GPU %f s, %f s\n", omp_get_wtime()-tt, ttinner);
+
+  double err = 0;
+  for (long i = 0; i < (N+2) * (N+2); i++) err += fabs(u_ref[i]-u_new[i]);
+  printf("Error = %f\n", err);
+
+  //printf("b[0] = %f\n", b[0]);
+
+  cudaFree(u_d);
+  cudaFree(u_new_d);
+
+  free(u);
+  free(u_new);
+  free(u_ref);
+  free(temp);
+
+  return 0;
 }
