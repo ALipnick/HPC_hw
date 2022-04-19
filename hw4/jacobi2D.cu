6176dd90dea463520476948f71bdc1c2bc6db99e
ALipnick <adl592@nyu.edu>
2022-04-19 18:22:35 -0400

    fixing jacobi

--- hw4/jacobi2D.cu
+++ hw4/jacobi2D.cu
@@ -7,7 +7,7 @@
 #include <string>
 #include <random>
 
-#define THREADS_PER_BLOCK 2
+#define THREADS_PER_BLOCK 32
 
 void jacobi2D_step(double* u_new, double* u, double h, long N) {
   #pragma omp parallel for collapse(2)
@@ -16,42 +16,46 @@ void jacobi2D_step(double* u_new, double* u, double h, long N) {
   		u_new[i+j*(N+2)] = 0.25*(h*h 
 			+ u[(i-1)+(j)*(N+2)] + u[(i)+(j-1)*(N+2)]
 			+ u[(i+1)+(j)*(N+2)] + u[(i)+(j+1)*(N+2)] );
+
+			}
+		}
+  #pragma omp parallel for collapse(2)
+  for (int i = 1; i<N+1; i++){
+  	for (int j = 1; j<N+1; j++){
+  		u[i+j*(N+2)] = u_new[i+j*(N+2)];
 			}
 		}
 }
 
 
 __global__ 
-void jacobi2D_step_kernel(double* u_new, double* u, double* temp, double h, long N) {
+void jacobi2D_step_kernel(double* u_new_d, double* u_d, double h, long N) {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   int j = blockIdx.y * blockDim.y + threadIdx.y;
   if ( (i > 0) && (i < N+1) && (j > 0) && (j < N+1) ) {
-  	u_new[i+j*(N+2)] = 0.25*(h*h 
-			+ u[(i-1)+(j)*(N+2)] + u[(i)+(j-1)*(N+2)]
-			+ u[(i+1)+(j)*(N+2)] + u[(i)+(j+1)*(N+2)] );
+  	u_new_d[i+j*(N+2)] = 0.25*(h*h 
+			+ u_d[(i-1)+(j)*(N+2)] + u_d[(i)+(j-1)*(N+2)]
+			+ u_d[(i+1)+(j)*(N+2)] + u_d[(i)+(j+1)*(N+2)] );
     __syncthreads();
-  	temp[i+j*(N+2)] = u[i+j*(N+2)];
-  	u[i+j*(N+2)] = u_new[i+j*(N+2)];
-  	u_new[i+j*(N+2)] = temp[i+j*(N+2)];
+  	u_d[i+j*(N+2)] = u_new_d[i+j*(N+2)];
   }
 }
 
 //decided to just run for full maximum iterations because just comparing two ways
-void jacobi2D(double* u_new, double* u,  double* temp, double h, long N, long max_iter) {
+void jacobi2D(double* u_new, double* u, double h, long N, long max_iter) {
 	for (long iter = 0; iter < max_iter; iter++) {
 		jacobi2D_step(u_new,  u,  h, N);
-		temp = u;
-	    u = u_new;
-	    u_new = temp;
+
 	}
 }
 
 
-void jacobi2D_cuda(double* u_new_d, double* u_d, double* temp_d, double h, long N, long max_iter) {
+void jacobi2D_cuda(double* u_new_d, double* u_d, double h, long N, long max_iter) {
   dim3 GridDim(N/THREADS_PER_BLOCK, N/THREADS_PER_BLOCK);
   dim3 BlockDim(THREADS_PER_BLOCK, THREADS_PER_BLOCK);
   for (long iter = 0; iter < max_iter; iter++) {
-  	jacobi2D_step_kernel<<<GridDim, BlockDim>>>(u_new_d,  u_d, temp_d, h, N);
+  	jacobi2D_step_kernel<<<GridDim, BlockDim>>>(u_new_d, u_d, h, N);
+
 	}
 }
 
@@ -80,12 +84,11 @@ void Check_CUDA_Error(const char *message){
 
 
 int main(void) {
-  int max_iter = 1000;
-  int N = 4;
+  int max_iter = 100;
+  int N = 1024;
  
   double* u = (double*) malloc((N+2) * (N+2) * sizeof(double)); // N+2 x N+2 grid with 0 boundary
   double* u_new = (double*) malloc((N+2) * (N+2) * sizeof(double));
-  double* temp = (double*) malloc((N+2) * (N+2) * sizeof(double));
   double* u_ref = (double*) malloc((N+2) * (N+2) * sizeof(double));
   #pragma omp parallel for schedule(static)
   for (long i = 0; i < (N+2)*(N+2); i++){ //initialize values
@@ -98,7 +101,7 @@ int main(void) {
   printf("residual = %f\n",residual);
   
   double tt = omp_get_wtime();
-  jacobi2D(u_new, u, temp, h, N,max_iter);
+  jacobi2D(u_new, u, h, N,max_iter);
 
   #pragma omp parallel for schedule(static)  
   for ( long i = 0; i < (N+2)*(N+2); i++ ) {
@@ -108,37 +111,31 @@ int main(void) {
   residual = calc_residual( u_ref, h, N);
   printf("residual = %f\n",residual);
 
-  double *u_d, *u_new_d, *temp_d;
+  double *u_d, *u_new_d;
   cudaMalloc(&u_d, (N+2) * (N+2) *sizeof(double));
   Check_CUDA_Error("malloc x failed");
   cudaMalloc(&u_new_d, (N+2) * (N+2) *sizeof(double));
-  cudaMalloc(&temp_d, (N+2) * (N+2) *sizeof(double));
 
   // reset u values
   #pragma omp parallel for schedule(static)
   for (long i = 0; i < (N+2)*(N+2); i++){ //initialize values
 		u[i] = 0;
-		u_new[i] = 0;
-		temp[i] = 0;
   }
 
   tt = omp_get_wtime();
   cudaMemcpy(u_d, u, (N+2) * (N+2) *sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(u_new_d, u_new, (N+2) * (N+2) * sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(temp_d, temp, (N+2) * (N+2) * sizeof(double), cudaMemcpyHostToDevice);
-
   double ttinner = omp_get_wtime();
-  jacobi2D_cuda(u_new_d, u_d, temp_d, h, N, max_iter);
-
+  jacobi2D_cuda(u_new_d, u_d, h, N, max_iter);
   cudaDeviceSynchronize();
   ttinner = omp_get_wtime() - ttinner;
-  cudaMemcpy(u_new, u_new_d, (N+2) * (N+2) * sizeof(double), cudaMemcpyDeviceToHost);
+  cudaMemcpy(u, u_d, (N+2) * (N+2) * sizeof(double), cudaMemcpyDeviceToHost);
   printf("GPU %f s, %f s\n", omp_get_wtime()-tt, ttinner);
-  residual = calc_residual(u_new, h, N);
+  
+  residual = calc_residual(u, h, N);
   printf("residual = %f\n",residual);
 
   double err = 0;
-  for (long i = 0; i < (N+2) * (N+2); i++) err += fabs(u_ref[i]-u_new[i]);
+  for (long i = 0; i < (N+2) * (N+2); i++) err += fabs(u_ref[i]-u[i]);
   printf("Error = %f\n", err);
 
   cudaFree(u_d);
@@ -147,7 +144,6 @@ int main(void) {
   free(u);
   free(u_new);
   free(u_ref);
-  free(temp);
 
   return 0;
 }
